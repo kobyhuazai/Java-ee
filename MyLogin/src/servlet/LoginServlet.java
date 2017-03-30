386f4f22cd1fafce348fe5ff6eca672358fefd29
kobyhuazai <aierqiaoshuya@gmail.com>
2017-03-30 09:21:32 +0800

    first

---
+++ MyLogin/src/servlet/LoginServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+  
+        System.out.println("name:" + name);
+        System.out.println("password:" + password);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+    	doPost(request,response);
+    }
+    
+}
